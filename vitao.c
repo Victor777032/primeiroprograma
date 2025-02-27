33b56d9456fd7f7f8772d08f91f37031fedecfaf
Victor Hugo <borges.campos@escola.pr.gov.br>
2025-02-27 11:57:17 -0300

    Add files via upload

---
+++ vitao.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+ int numero = 5;
+ int chute;
+ 
+ printf(" digite um numero: ");
+ scanf("%d" , &chute);
+  
+  if(chute == numero){
+     printf("acertou! \n"); 
+  }else{
+      printf(" errou, bobão! \n");
+  }
+  printf("o numero era %d" , numero);
+  
+    return 0;
+}
